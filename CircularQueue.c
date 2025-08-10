26e3ffbc7e9e0df6c7db47eea4c933e351cf46c5
Alwin Jose George <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2025-08-10 21:08:33 +0530

    Update CircularQueue.c

--- CircularQueue.c
+++ CircularQueue.c
@@ -1,70 +1,79 @@
 #include <stdio.h>
-#define MAX 5
 
-int queue[MAX];
-int front = -1, rear = -1;
+int FRONT=0 ,REAR = -1, size=0, capacity;
 
-void enqueue(int item) {
-    if ((front == 0 && rear == MAX - 1) || (front == rear + 1)) {
-        printf("Queue Overflow\n");
-        return;
-    }
-    if (front == -1) {
-        front = rear = 0;
-    } else if (rear == MAX - 1) {
-        rear = 0;
-    } else {
-        rear++;
-    }
-    queue[rear] = item;
-}
-
-void dequeue() {
-    if (front == -1) {
-        printf("Queue Underflow\n");
-        return;
-    }
-    printf("Deleted element: %d\n", queue[front]);
-    if (front == rear) {
-        front = rear = -1;
-    } else if (front == MAX - 1) {
-        front = 0;
-    } else {
-        front++;
+void EnQ(int CircularQ[],int value){
+    if(size==capacity){return;}
+    else{
+        REAR = (FRONT + size)%capacity;
+        CircularQ[REAR]=value;
+        size++;
     }
 }
 
-void display() {
-    int i;
-    if (front == -1) {
-        printf("Queue is empty\n");
-        return;
+void DeQ(){
+    if(size==0){return;}
+    if(FRONT==REAR){
+        FRONT=0;
+        REAR =-1;
     }
-    printf("Queue: ");
-    if (front <= rear) {
-        for (i = front; i <= rear; i++)
-            printf("%d ", queue[i]);
-    } else {
-        for (i = front; i < MAX; i++)
-            printf("%d ", queue[i]);
-        for (i = 0; i <= rear; i++)
-            printf("%d ", queue[i]);
+    else{
+        FRONT = (FRONT+1)%capacity;
     }
-    printf("\n");
+    size--;
 }
 
-int main() {
-    enqueue(10);
-    enqueue(20);
-    enqueue(30);
-    display();
-    dequeue();
-    display();
-    enqueue(40);
-    enqueue(50);
-    enqueue(60);
-    display();
-    dequeue();
-    display();
-    return 0;
+void display(int CircularQ[]){
+    printf("Circular Queue: ");
+    if(FRONT<=REAR){
+        for(int i=FRONT;i<=REAR;i++){
+            printf("%d\t",CircularQ[i]);
+        }
+    }
+    else{
+        for(int i=FRONT;i<=capacity-1;i++){
+            printf("%d\t",CircularQ[i]);
+        }
+        for(int i=0;i<=REAR;i++){
+            printf("%d\t",CircularQ[i]);
+        }
+    }
+}
+
+int main(){
+    printf("\nEnter the size of Circular Queue: ");
+    scanf("%d",&capacity);
+
+    int CircularQ[capacity];
+
+
+while(1){
+        int choice,value;
+        printf("\n---------!Select queue option!---------\n");
+        printf("1. Enqueue\t2. Dequeue\t3. Exit\nChoice: ");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1:if(size==capacity){
+                printf("Queue Overflow!\n");
+            }else{
+                printf("\nEnter the element: \n");
+                scanf("%d",&value);
+                EnQ(CircularQ,value);
+                display(CircularQ);
+            }
+            break;
+            case 2:if(size==0){
+                printf("Queue Empty!\n");
+            }else{
+                DeQ();
+                display(CircularQ);
+            }
+            break;
+            case 3: return 0;
+            default:printf("Invalid choice!");
+        }
+
+    }
+    
 }
